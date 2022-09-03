e6a25886b91d65056b341aa419ca57e775699dbf
Filip Stepien <filip.stepien@gmail.com>
2022-09-03 15:26:44 -0400

    mars weight

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter you weight (kg):");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+
+    let mut mars_weight = calculate_weight_on_mars(weight);
+    mars_weight = mars_weight * 1000.0;
+    println!("Weight on Mars: {}g", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
